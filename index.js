2f54f6c83739ee1c36d24d88ec6a5f1b3f393694
brendan <b.chatterton@lifewisedigital.com>
2023-05-01 19:49:06 -0400

    tab comment pseudo ish code instructions

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 import csv from 'csv-parser';
 import fs from 'fs';
+
+// Take folder name path from args
+
+// foreach file
+    // open the file in a stream
+        // Edit csv file column 'imgSrc' using node
+    // execute shell command
+        // mongoimport --uri "mongodb+srv://davie-deals-node-server:cvTrNbdzA7sAvNIj@davie-deals.ltkcsre.mongodb.net/development?authSource=admin&replicaSet=atlas-ofdyqw-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true" --collection deals --headerline --type csv --file <concatenate or interpolate file as last argument of this shell command here>
