6e296ff4f3f76740512da7117f1ed6b40e1b328a
brendan <b.chatterton@lifewisedigital.com>
2023-05-03 14:18:18 -0400

    clean up a bit

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import csv from 'csv-parser';
 import fs from 'fs';
 import yargs from 'yargs';
-import { AsyncParser } from '@json2csv/node';
-import stripBomStream from 'strip-bom-stream';
-import { execSync, exec } from 'child_process';
-import path from 'path';
+import { exec } from 'child_process';
 
 const options = yargs(process.argv.slice(2))
  .usage("Usage: --csv <path>")
@@ -14,35 +10,29 @@ const options = yargs(process.argv.slice(2))
  // Take folder name path from args
 const folder = `${options.csv}`;
 
-console.log('NEW RUN \n\n\n\n\n\n\n\n');
-
-
 fs.readdir(folder, (err, files) => {
     files.forEach((file) => { 
       
         const filePath = `${folder}/${file}`;
         
         let cleanImgSrc;
-        fs.readFile(filePath,'utf8',  (err,data) =>{
+        fs.readFile(filePath,'utf8',  (_, data) =>{
            
             var dataArray = data.split(',');
             var noEmptyFields = dataArray.filter(x => x != '');
             cleanImgSrc = noEmptyFields.map((x) => {
-                if(x.includes('img src=')) {
-                    
-                    
+                if(x.includes('img src=')) {    
                  var match = x.match(/img src[^=]*=""([^""]+)""/);
            
-                 return match ? match[1] : x; 
+                 if(match) return match[1]; 
                 }
+
                 return x;
             });
             console.log(`FILE PATH: ${filePath}`);
             fs.writeFile(filePath, cleanImgSrc.join(','), null, ()=>{});
             exec(`mongoimport --uri "mongodb+srv://davie-deals-node-server:cvTrNbdzA7sAvNIj@davie-deals.ltkcsre.mongodb.net/development?authSource=admin&replicaSet=atlas-ofdyqw-shard-0&ssl=true" --collection deals --headerline --type csv ${filePath}`);
-        });
-
-        
+        });        
     });
 });
     
