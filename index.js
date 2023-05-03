1cde132aa360add2d7869389dd6f7080566acc43
brendan <b.chatterton@lifewisedigital.com>
2023-05-03 13:04:37 -0400

    as array

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import fs from 'fs';
 import yargs from 'yargs';
 import { AsyncParser } from '@json2csv/node';
 import stripBomStream from 'strip-bom-stream';
-import { execSync } from 'child_process';
+import { execSync, exec } from 'child_process';
+import path from 'path';
 
 const options = yargs(process.argv.slice(2))
  .usage("Usage: --csv <path>")
@@ -13,39 +14,28 @@ const options = yargs(process.argv.slice(2))
  // Take folder name path from args
 const folder = `${options.csv}`;
 
+console.log('NEW RUN \n\n\n\n\n\n\n\n');
 
 fs.readdir(folder, (err, files) => {
     files.forEach((file) => { 
-      // Scope ensures each file goes into a single data array instead of all files into one data array.
-      const fileData = []; 
-      const filePath = `${folder}//${file}`;
-fs.createReadStream(filePath)
-  .pipe(stripBomStream())
-  .pipe(csv({mapValues: ({header, index, value}) => {
-    if(index == 0 && header == '') throw Error('Empty row')
-    // Use regex capture group (located at index 1 of array) to replace imgSrc tag with url.
-    if(header == 'imgSrc') {
-      const regexMatch = value.toString().match(/img src[^=]*="([^"]+)"/);
-      return regexMatch ? regexMatch[1] : value;
-    };
-    return value;
-  }}))
-  .on("data", function (row) {
-    fileData.push(row);
+      
+        const filePath = `${folder}//${file}`;
+        
 
-  })
-  .on("end", async function () {
-    // AsyncParser parses the fileData and pipes it out to the file via the write stream.
-    new AsyncParser().parse(fileData).pipe(fs.createWriteStream(filePath))
-    const command = `mongoimport --uri "mongodb+srv://davie-deals-node-server:cvTrNbdzA7sAvNIj@davie-deals.ltkcsre.mongodb.net/development?authSource=admin&replicaSet=atlas-ofdyqw-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true" --collection deals --headerline --type csv --file ${filePath}`;
-    execSync(command);
-
-  })
-  .on("error", (err) => console.log(err))
+        fs.readFile(filePath,'utf-8',  (err,data) =>{
+           
+            var dataArray = data.split(',');
+            var noExtraCommas = dataArray.filter(x => x != '');
+            var cleanImgSrc = noExtraCommas.map((x) => {
+                if(x.includes('img src=')) {
+                 console.log(x.match(/img src[^=]*="([^"]+)"/));
+                 return null
+                }
+                return x;
+            });
+            //console.log(cleanImgSrc);
+        
+        });
+    });
 });
-});
-// foreach file
-    // open the file in a stream
-        // Edit csv file column 'imgSrc' using node
-    // execute shell command
-        // mongoimport --uri "mongodb+srv://davie-deals-node-server:cvTrNbdzA7sAvNIj@davie-deals.ltkcsre.mongodb.net/development?authSource=admin&replicaSet=atlas-ofdyqw-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true" --collection deals --headerline --type csv --file <concatenate or interpolate file as last argument of this shell command here>
+    
